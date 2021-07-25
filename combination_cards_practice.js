d1598d0fa5c3b6b6238a55c1c32cd3e2aab82968
TAETAEHO <xogh9204@gmail.com>
2021-07-25 18:27:16 +0900

    add : another solution

--- combination_cards_practice.js
+++ combination_cards_practice.js
@@ -1,9 +1,28 @@
 /**
  * 카드를 입력받아 카드 배열의 요소 중에서 3가지를 조합하여 합한 숫자를 만든다.
  * 조합한 합 중에서 소수의 개수를 리턴한다.
+// 소수 판별 함수 2
+// const isPrime = (num) => {
+//   // 2는 소수다
+//   if (num === 2) {
+//     return true;
+//   }
+
+//   if (num % 2 === 0) {
+//     return false;
+//   }
+
+//   for (let i = 3; i < num; i++) {
+//     // 나누어 떨어지는 경우는 소수가 아니다
+//     if (num % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
  */
 
-// Solution
+// Solution 1
 function blackJack(cards) {
   // cards의 요수중에서 3가지로 조합할수 있는 경우의 수를 담을 변수를 생성한다.
   let combination = [];
@@ -51,22 +70,41 @@ const isPrime = (num) => {
 
 console.log(blackJack([1, 2, 3, 4]));
 
-// 소수 판별 함수 2
-// const isPrime = (num) => {
-//   // 2는 소수다
-//   if (num === 2) {
-//     return true;
-//   }
+// Solution 2
+function blackJack(cards) {
+  // 소수 개수 판별 cnt
+  let cnt = 0;
 
-//   if (num % 2 === 0) {
-//     return false;
-//   }
+  // cards를 탐색하면서 요소 3개를 조합한다.
+  for (let i = 0; i < cards.length; i++) {
+    for (let j = i + 1; j < cards.length; j++) {
+      for (let k = j + 1; k < cards.length; k++) {
+        // i, j, k번째 요소를 더해서 소수이면 카운트 증가시켜준다.
+        // 최종적으로 소수의 개수만 리턴하면 문제해결
+        if (isPrime(cards[i] + cards[j] + cards[k])) {
+          cnt++;
+        }
+      }
+    }
+  }
 
-//   for (let i = 3; i < num; i++) {
-//     // 나누어 떨어지는 경우는 소수가 아니다
-//     if (num % i === 0) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
+  return cnt;
+}
+
+// 소수를 판별하기 위한 함수
+const isPrime = (num) => {
+  // 2는 소수다
+  if (num === 2) {
+    return true;
+  }
+
+  for (let i = 2; i <= Math.floor(Math.sqrt(num)); i++) {
+    // 나누어 떨어지는 경우는 소수가 아니다
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(blackJack([1, 2, 3, 4]));
