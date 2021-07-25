b139ec13b2b77b00155857959bd0fa6ff1437846
TAETAEHO <xogh9204@gmail.com>
2021-07-25 14:58:08 +0900

    add : rockPaperScissors_practice

---
+++ rockScissorsPaper_practice.js
@@ -0,0 +1,37 @@
+/**
+ * 2인 이상
+ * n 판의 가위바위보 게임을 할 경우, 한 사람은 세 번의 선택(예. 가위, 가위, 보)을 할 수 있습니다.
+ * 세 번의 선택으로 가능한 모든 경우의 수를 구하는 함수를 작성합니다.
+ */
+
+// Solution
+function rockScissorsPaper(rounds) {
+  // rounds 설정
+  rounds = rounds || 3;
+  // return할 최종 배열 변수
+  let result = [];
+  // lookup table
+  let rps = ["rock", "paper", "scissors"];
+
+  // 재귀 함수 생성
+  let recursive = (rounds, games) => {
+    for (let i = 0; i < rps.length; i++) {
+      let currentRps = rps[i];
+
+      // base case
+      if (rounds === 0) {
+        result.push(games);
+        return;
+      }
+
+      // 재귀
+      recursive(rounds - 1, games.concat(currentRps));
+    }
+  };
+
+  // 재귀 함수 호출 -> 경우의 수 초기값 []
+  recursive(rounds, []);
+  return result;
+}
+
+console.log(rockScissorsPaper(5));
