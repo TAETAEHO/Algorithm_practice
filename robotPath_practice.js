d6f0ae1a77b264c67bfda988ec2b51c7cb48a71e
TAETAEHO <xogh9204@gmail.com>
2021-08-11 09:14:45 +0900

    add : another solution

--- robotPath_practice.js
+++ robotPath_practice.js
@@ -6,7 +6,7 @@
  * 큐로 문제를 접근한다.
  */
 
-// Solution
+// Solution 1
 const robotPath = (room, src, dst) => {
   // 지나왔던 길인지 아닌지 판별하는 변수
   const visited = new Array(room.length).fill(0).map(() => new Array());
@@ -41,6 +41,52 @@ const robotPath = (room, src, dst) => {
       if (dY === dst[0] && dX === dst[1]) return result[2];
     }
   }
+};
+
+// Solution 2
+const robotPath = (room, src, dst) => {
+  // 출발지
+  room[src[0]][src[1]] = 1;
+
+  // 세로, 가로 길이
+  let M = room.length - 1;
+  let N = room[0].length - 1;
+  let DIR = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+  ];
+
+  // 유효성 검사
+  let isValid = (y, x) => y >= 0 && y <= M && x >= 0 && x <= N;
+
+  let queue = [src];
+
+  // 목적지까지 움직이는 함수
+  let moveDirection = (curDir) => {
+    const [Y, X] = curDir;
+
+    // 상하좌우
+    for (let i = 0; i < 4; i++) {
+      let dY = Y + DIR[i][0];
+      let dX = X + DIR[i][1];
+
+      // 유효성 검사
+      if (isValid(dY, dX) && room[dY][dX] === 0) {
+        queue.push([dY, dX]);
+
+        room[dY][dX] = room[Y][X] + 1;
+      }
+    }
+  };
+
+  while (queue.length > 0) {
+    let curDir = queue.shift();
+    moveDirection(curDir);
+  }
+
+  return room[dst[0]][dst[1]] - 1;
 };
 
 console.log(
