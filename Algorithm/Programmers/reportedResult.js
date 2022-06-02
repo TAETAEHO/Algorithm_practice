72ba97c7656e244889a4cd2e0c3b931b6cc8a0a6
TAETAEHO <xogh9204@gmail.com>
2022-06-02 11:28:57 +0900

    신고결과받기 reference

--- Algorithm/Programmers/reportedResult.js
+++ Algorithm/Programmers/reportedResult.js
@@ -1,4 +1,5 @@
 /**
+ * 프로그래머스 lv.1 - 신고 결과 받기
  * 신입사원 무지는 게시판 불량 이용자를 신고하고 처리 결과를 메일로 발송하는 시스템을 개발하려 합니다. 무지가 개발하려는 시스템은 다음과 같습니다.
  * 각 유저는 한 번에 한 명의 유저를 신고할 수 있습니다.
  * 신고 횟수에 제한은 없습니다. 서로 다른 유저를 계속해서 신고할 수 있습니다.
@@ -11,47 +12,80 @@
  */
 
 // Solution
-function reportedResult(id_list, report, k) {
-  let withoutDupReport = [...new Set(report)]; // 중복 제거
-  let reporteeCnt = 0;
-  let arr = [];
-  let obj = {};
-  let idOBJ = {};
-  let newReport = {};
+// function reportedResult(id_list, report, k) {
+//   let withoutDupReport = [...new Set(report)]; // 중복 제거
 
-  if (withoutDupReport.length < k) return [0, 0];
+//   let reporterARR = [];
+//   let reporterOBJ = {};
 
-  for (let i = 0; i < id_list.length; i++) {
-    idOBJ = {
-      ...idOBJ,
-      [id_list[i]]: reporteeCnt,
-    };
+//   let reporteeCnt = 0;
+//   let reporteeARR = [];
+//   let reporteeOBJ = {};
+
+//   if (withoutDupReport.length < k) return [0, 0];
+
+//   for (let i = 0; i < withoutDupReport.length; i++) {
+//     let reporter = withoutDupReport[i].split(" ")[0];
+//     let reportee = withoutDupReport[i].split(" ")[1];
+
+//     reporteeOBJ = {
+//       [reportee]: reporteeCnt,
+//     };
+
+//     reporterOBJ = {
+//       [reporter]: reportee,
+//     };
+
+//     reporteeARR.push(reporteeOBJ);
+//     reporterARR.push(reporterOBJ);
+
+//     if (
+//       Object.keys(reporteeARR[i]).toString() ==
+//       Object.values(reporterARR[i]).toString()
+//     ) {
+//       // console.log("2222222222");
+//     }
+//   }
+
+//   console.log(reporterARR);
+//   console.log(reporteeARR);
+
+//   return withoutDupReport;
+// }
+
+function solution(id_list, report, k) {
+  const result = new Array(id_list.length).fill(0);
+  const reported_list = {};
+
+  id_list.map((user) => {
+    reported_list[user] = [];
+  });
+
+  report.map((user) => {
+    const [user_id, reported_id] = user.split(" ");
+
+    if (!reported_list[reported_id].includes(user_id)) {
+      reported_list[reported_id].push(user_id);
+    }
+  });
+
+  for (const key in reported_list) {
+    if (reported_list[key].length >= k) {
+      reported_list[key].map((user) => {
+        result[id_list.indexOf(user)] += 1;
+      });
+    }
   }
-
-  for (let i = 0; i < withoutDupReport.length; i++) {
-    let reporter = withoutDupReport[i].split(" ")[0];
-    let reportee = withoutDupReport[i].split(" ")[1];
-
-    newReport = {
-      [reporter]: reportee,
-    };
-    arr.push(newReport);
-
-    // if(Object.values(arr[i]))
-  }
-
-  // console.log(arr);
-  console.log(idOBJ);
-  return withoutDupReport;
+  return result;
 }
 
-// reportedResult(
+// solution(
 //   ["con", "ryan"],
 //   ["ryan con", "ryan con", "ryan con", "ryan con"],
 //   3
 // );
 
-reportedResult(
+solution(
   ["muzi", "frodo", "apeach", "neo"],
   ["muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"],
   2
