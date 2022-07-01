95d42eb574f029521ea4940aa06a164853045c13
TAETAEHO <xogh9204@gmail.com>
2022-07-01 15:35:55 +0900

    두개 뽑아 더하기

--- Algorithm/Programmers/addNumbers.js
+++ Algorithm/Programmers/addNumbers.js
@@ -6,22 +6,32 @@
  */
 
 function solution(numbers) {
-  numbers.sort((a, b) => a - b);
   let sum = 0;
   let result = [];
 
   for (let i = 0; i < numbers.length; i++) {
-    for (let j = i + 1; j < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
       sum = numbers[i] + numbers[j];
 
-      if (result.includes(sum)) continue;
-      result.push(sum);
+      if (!result.includes(sum)) result.push(sum);
     }
   }
-  console.log(result);
 
-  return result;
+  return result.sort((a, b) => a - b);
 }
 
-solution([2, 1, 3, 4, 1]); // [2,3,4,5,6,7]
+function solution2(numbers) {
+  let result = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      result.push(numbers[i] + numbers[j]);
+    }
+  }
+
+  let sum = [...new Set(result)];
+  return sum.sort((a, b) => a - b);
+}
+
+solution2([2, 1, 3, 4, 1]); // [2,3,4,5,6,7]
 // [1, 1, 2, 3, 4]
