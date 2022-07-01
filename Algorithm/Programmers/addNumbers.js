b4584dfff46a97682cd9e69558d37e2f33c78cc9
TAETAEHO <xogh9204@gmail.com>
2022-07-01 15:12:50 +0900

    레벨 1 : 두개 뽑아서 더하기

---
+++ Algorithm/Programmers/addNumbers.js
@@ -0,0 +1,24 @@
+/**
+ * 프로그래머스 lv.1 - 두 개 뽑아서 더하기
+ * 정수 배열 numbers가 주어집니다.
+ * numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서
+ * 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
+ */
+
+function solution(numbers) {
+  numbers.sort((a, b) => a - b);
+  let sum = 0;
+  let result = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; i++) {
+      if (result.includes(sum)) continuel;
+
+      sum = numbers[i] + numbers[j];
+      result.push(sum);
+    }
+  }
+}
+
+solution([2, 1, 3, 4, 1]); // [2,3,4,5,6,7]
+// [1, 1, 2, 3, 4]
