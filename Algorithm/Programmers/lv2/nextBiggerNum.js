17cf608925ad4c18b6ec15c5cb9766b061910e7f
TAETAEHO <xogh9204@gmail.com>
2022-10-05 11:07:26 +0900

    programmers lv2

---
+++ Algorithm/Programmers/lv2/nextBiggerNum.js
@@ -0,0 +1,33 @@
+/**
+ * 프로그래머스 lv.2 - 다음 큰 숫자
+ * 자연수 n이 주어졌을 때, n의 다음 큰 숫자는 다음과 같이 정의 합니다.
+ *
+ * 조건 1. n의 다음 큰 숫자는 n보다 큰 자연수 입니다.
+ * 조건 2. n의 다음 큰 숫자와 n은 2진수로 변환했을 때 1의 갯수가 같습니다.
+ * 조건 3. n의 다음 큰 숫자는 조건 1, 2를 만족하는 수 중 가장 작은 수 입니다.
+ * 예를 들어서 78(1001110)의 다음 큰 숫자는 83(1010011)입니다.
+ *
+ * 자연수 n이 매개변수로 주어질 때, n의 다음 큰 숫자를 return 하는 solution 함수를 완성해주세요.
+ */
+
+function solution(n) {
+  let nBinary = n.toString(2);
+  let oneCnt = nBinary.split("").filter((el) => el === "1").length;
+  let nextNum = n;
+  let nextOneCnt = 0;
+
+  while (oneCnt) {
+    n++;
+    nextNum += 1;
+    nextOneCnt = n
+      .toString(2)
+      .split("")
+      .filter((el) => el === "1").length;
+
+    if (oneCnt === nextOneCnt) break;
+  }
+
+  return nextNum;
+}
+
+solution(78);
