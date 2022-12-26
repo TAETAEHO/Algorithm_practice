91ebec71740a94e461bb6cf7446f0ee7df820244
TAETAEHO <xogh9204@gmail.com>
2022-12-26 10:33:33 +0900

    add : abstraction

--- TypeScript/3-oop/3-4-abstraction.ts
+++ TypeScript/3-oop/3-4-abstraction.ts
@@ -9,8 +9,14 @@
     makeCoffee(shots: number): CoffeeCup;
   }
 
+  interface CommercialCoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+    fillCoffeeBeans(beans: number): void;
+    clean(): void;
+  }
+
   // 인터페이스를 구현하는 부분 -> 인터페이스에서 작성한 함수를 모두 구현한다.
-  class CoffeeMachine implements CoffeeMaker {
+  class CoffeeMachine implements CoffeeMaker, CommercialCoffeeMaker {
     private static BEANS_GRAMM_PER_SHOT: number = 7; // class level
     private coffeeBeans: number = 0; // instance(object) level
 
@@ -30,6 +36,10 @@
         throw new Error("value for beans should be greater than 0");
       }
       this.coffeeBeans += beans;
+    }
+
+    clean(): void {
+      console.log("cleaning the machine...");
     }
 
     private grindBeans(shots: number) {
@@ -60,13 +70,40 @@
     }
   }
 
+  class AmateurUser {
+    constructor(private machine: CoffeeMaker) {}
+
+    makeCoffee() {
+      const coffee = this.machine.makeCoffee(2);
+      console.log(coffee);
+    }
+  }
+
+  class ProBarista {
+    constructor(private machine: CommercialCoffeeMaker) {}
+
+    makeCoffee() {
+      const coffee = this.machine.makeCoffee(2);
+      console.log(coffee);
+      this.machine.fillCoffeeBeans(45);
+      this.machine.clean();
+    }
+  }
+
   // 생성자 호출
   // 추상화 -> 정말 필요한 함수만 노출
+  // CoffeeMachine이라는 타입으로 CoffeeMachine object를 받게되면 public으로 되어있는 함수들을 사용할 수 있다.
   const maker: CoffeeMachine = CoffeeMachine.makeMachine(32);
   maker.fillCoffeeBeans(32);
   maker.makeCoffee(2);
 
-  const maker2: CoffeeMaker = CoffeeMachine.makeMachine(32);
+  // interface로 타입을 제한하면 interface에서 사용된 함수들만 사용할 수 있다.
+  const maker2: CommercialCoffeeMaker = CoffeeMachine.makeMachine(32);
   maker2.fillCoffeeBeans(32); // maker의 타입은 CoffeeMaker이며 CoffeeMaker interface에 작성되지 않은 함수는 사용할 수 없다. (추상화)
   maker2.makeCoffee(2);
+  maker2.clean();
+
+  const maker3: CoffeeMachine = CoffeeMachine.makeMachine(32);
+  const amateur = new AmateurUser(maker3);
+  const pro = new ProBarista(maker3);
 }
